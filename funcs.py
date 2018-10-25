f1a30cb12f028d2fe38aad8ebde958eb4d45226e
Dana Geislinger <dg332@nau.edu>
2018-10-24 19:50:01 -0700

    Fixed datetime issues, created "save_df" helper function

--- funcs.py
+++ funcs.py
@@ -10,3 +10,8 @@ def load_df():
         pickled_df = pickle.load(pkl)
 
     return pickled_df
+
+def save_df(df):
+    """ Helper function to save df (pd.DataFrame) as 'data/df.pkl'. """
+    with open('data/df.pkl', 'wb') as pkl:
+        pickle.dump(df, pkl)
